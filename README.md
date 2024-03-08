214e2da5450d3030c7b87f6092e527e20ca64984
pochemoo <boxofmonster@mail.ru>
2024-03-08 22:24:55 +0500

    re make name of classes and design

---
+++ README.md
@@ -0,0 +1,3 @@
+# TEXT REVERSE
+
+Text reverse is a static web site that rewraps your text. This site is located at this address [word_reverse](https://secretlodge.github.io/word_reverse/).
--- scripts/index.js
+++ scripts/index.js
@@ -1,40 +1,31 @@
 /*
+    - БЭМ
+    - Адаптивный дизай
+    - Семантическая верстка
     - Анимация медленного появления при обновлении страницы
-    - Максимально ограничение 200 символов
-        - Pending
-        - Success 
-        - Error
-    - lazy load
-    - Сделать фильтр на HTML символы
-    - Переключение на светлую и темную тему 
-    - Переключение языков
-    - Автоматическое копирование в буфер обмена и всплывающее модальное окно которое об это мообщает
 */
 const result = document.getElementById('result')
 const button = document.getElementById('button')
 const input = document.getElementById('input')
 
-const longRuntimeSimulation = (seconds) => {
-    return new Promise((resolve) => [
+const SECONDS_OF_DELAY = 1;
+
+const delaySimulation = (seconds) => {
+    return new Promise((resolve) => {
         setTimeout(() => {
             resolve(true)
         }, seconds * 1000)
-    ])
+    })
 }
 
 const reverseWord = async ({ code }) => { 
     if(code && code === 'Enter' || !code) {
         button.classList.add('pending')
-        
-        const text = input.value.split('')
+        await delaySimulation(SECONDS_OF_DELAY)
 
-        await longRuntimeSimulation(2)
-
-        if(text.length > 1000) {
-            // Границы поля ввода граснеют и всплывает подсказка, что максимальное кол-во символов это 1000
-        }
-        else {
-            result.textContent = text
+        if(input.value.length !== 0) {
+            result.textContent = input.value
+                .split('')
                 .reverse()
                 .join('')
         }
